faa20e065ffbe4c208967a95033a016f8a8775ff
Tibor Schneider <tiborschneider@bluewin.ch>
2020-03-09 11:53:38 +0100

    Added performance measurement test, updated measurements

---
+++ test/cl/mrbci/logm_parallel/cluster.c
@@ -0,0 +1,125 @@
+#include "stdio.h"
+#include "rt/rt_api.h"
+#include "../../../../src/cl/mrbci/mrbci.h"
+#include "test_stimuli.h"
+
+#define NUM_WORKERS 8
+#define _LOGM_WORKSPACE_SIZE (MRBCI_C * (2 * MRBCI_C + 2))
+
+// extern definitions, since those are not exported in mrbci.h
+typedef struct {
+    int32_t* p_whitened;
+    float* p_logm_workspace;
+    int8_t* p_logm_out;
+    int8_t* p_out;
+    unsigned int start_freq_idx;
+} _mrbci_feature_extraction_kernel_instance_t;
+void _mrbci_feature_extraction_kernel(void* args);
+
+RT_CL_DATA static float* workspace_l1;
+
+int do_bench(rt_perf_t* perf, int events) {
+    //setup performance measurement
+    rt_perf_conf(perf, events);
+
+    int8_t* y_acq = rt_alloc(RT_ALLOC_FC_DATA, sizeof(int8_t) * MRBCI_SVM_NUM_FEATURES_ALIGN);
+    rt_dma_copy_t _copy;
+
+    // start performance measurement
+    rt_perf_reset(perf);
+    rt_perf_start(perf);
+
+
+
+    int32_t* _p_whitened_l1_th = rt_alloc(RT_ALLOC_CL_DATA, sizeof(int32_t) * MRBCI_C * MRBCI_C * NUM_WORKERS);
+    float* _p_logm_workspace_l1_th = rt_alloc(RT_ALLOC_CL_DATA, sizeof(float) * _LOGM_WORKSPACE_SIZE * NUM_WORKERS);
+    int8_t* _p_logm_out_l1_th = rt_alloc(RT_ALLOC_CL_DATA, sizeof(int8_t) * MRBCI_C * MRBCI_C_ALIGN * NUM_WORKERS);
+    int8_t* _p_out_l1_th = rt_alloc(RT_ALLOC_CL_DATA, sizeof(int8_t) * MRBCI_HALF_DIAG_FEATURES * NUM_WORKERS);
+
+    int _merge = 0;
+
+    for (unsigned int _start_freq_idx = 0; _start_freq_idx < MRBCI_NUM_FREQ; _start_freq_idx += NUM_WORKERS) {
+
+        unsigned int _batch_size = MRBCI_NUM_FREQ - _start_freq_idx;
+        if (_batch_size > NUM_WORKERS) {
+            _batch_size = NUM_WORKERS;
+        }
+
+        // copy the whitened data to l1
+        rt_dma_memcpy((unsigned int)(x_stm + _start_freq_idx * MRBCI_C * MRBCI_C),
+                      (unsigned int)_p_whitened_l1_th,
+                      sizeof(int32_t) * MRBCI_C * MRBCI_C * _batch_size,
+                      RT_DMA_DIR_EXT2LOC, _merge, &_copy);
+
+        // prepare the arguments for the kernel
+        _mrbci_feature_extraction_kernel_instance_t _args;
+        _args.p_whitened = _p_whitened_l1_th;
+        _args.p_logm_workspace = _p_logm_workspace_l1_th;
+        _args.p_logm_out = _p_logm_out_l1_th;
+        _args.p_out = _p_out_l1_th;
+        _args.start_freq_idx = _start_freq_idx;
+
+        // wait until the dma is finished
+        rt_dma_wait(&_copy);
+
+        // call the cluster
+        rt_team_fork(NUM_WORKERS, _mrbci_feature_extraction_kernel, &_args);
+
+        // copy the data back to the result
+        rt_dma_memcpy((unsigned int)(y_acq + MRBCI_HALF_DIAG_FEATURES * _start_freq_idx),
+                      (unsigned int)_p_out_l1_th,
+                      sizeof(int8_t) * MRBCI_HALF_DIAG_FEATURES * _batch_size,
+                      RT_DMA_DIR_LOC2EXT, 0, &_copy);
+
+        // remember that the next memcopy to l1 must be merged
+        _merge = 1;
+
+    }
+
+    // wait until the data is copied
+    rt_dma_wait(&_copy);
+
+    // free the data required in the kernel
+    rt_free(RT_ALLOC_CL_DATA, _p_whitened_l1_th, sizeof(int32_t) * MRBCI_C * MRBCI_C * NUM_WORKERS);
+    rt_free(RT_ALLOC_CL_DATA, _p_logm_workspace_l1_th, sizeof(float) * _LOGM_WORKSPACE_SIZE * NUM_WORKERS);
+    rt_free(RT_ALLOC_CL_DATA, _p_logm_out_l1_th, sizeof(int8_t) * MRBCI_C * MRBCI_C_ALIGN * NUM_WORKERS);
+    rt_free(RT_ALLOC_CL_DATA, _p_out_l1_th, sizeof(int8_t) * MRBCI_HALF_DIAG_FEATURES * NUM_WORKERS);
+
+
+
+    rt_perf_stop(perf);
+
+    int error = 0;
+    for (int i = 0; i < MRBCI_SVM_NUM_FEATURES; i++) {
+            if (y_acq[i] != y_exp[i]) {
+                error = 1;
+                //printf("error at: %d: acq=%d, exp=%d\n", i, y_acq[i], y_exp[i]);
+            }
+    }
+
+    rt_free(RT_ALLOC_FC_DATA, y_acq, sizeof(int8_t) * MRBCI_SVM_NUM_FEATURES_ALIGN);
+
+    return error;
+}
+
+void cluster_entry(void* arg) {
+
+    // setup performance measurement
+    rt_perf_t perf;
+    rt_perf_init(&perf);
+
+    mrbci_init();
+
+    int result;
+
+    result = do_bench(&perf, (1<<RT_PERF_CYCLES | 1<<RT_PERF_INSTR));
+
+    // print the results
+    if (result == 0) {
+        printf("## 1: result: OK\n");
+    } else {
+        printf("## 1: result: FAIL\n");
+    }
+    printf("## 1: cycles: %d\n", rt_perf_read(RT_PERF_CYCLES));
+    printf("## 1: instructions: %d\n", rt_perf_read(RT_PERF_INSTR));
+}
---
+++ test/cl/mrbci/logm_parallel/testcase.py
@@ -0,0 +1,97 @@
+"""
+This file will test the convolution implementation
+"""
+
+import random
+import os
+import numpy as np
+import pickle
+from test_utils import parse_output, TestLogger
+from header_file import HeaderFile, HeaderConstant, HeaderArray, HeaderInclude, HeaderScalar, \
+    align_array, align_array_size
+from golden_model import GoldenModel
+from makefile import Makefile
+import functional as F
+
+TESTNAME = "cl::mrbci::feature_extraction_kernel"
+RESULT_FILE = "result.out"
+
+MODEL_FILENAME = "../../../../data/model.pkl"
+DATA_FILENAME = "../../../../data/verification.pkl"
+
+
+def gen_stimuli():
+    """
+    This function generates the stimuli
+    """
+    model = GoldenModel(MODEL_FILENAME)
+
+    inp_block = model.feature_extraction.freq_band[0].logm
+    out_block = model.feature_extraction.freq_band[0].half_diag
+
+    with open(DATA_FILENAME, 'rb') as _f:
+        data = pickle.load(_f)
+
+    X = F.quantize_to_int(data['cov_mat_transform'], inp_block.input_scale,
+                          inp_block.input_n_bits, do_round=True)
+    Y = F.quantize_to_int(data['features_quant'], out_block.output_scale,
+                          out_block.output_n_bits, do_round=True)
+
+    return X, Y
+
+
+def test():
+    """
+    Execute the tests
+    Returns: (n_total, n_success)
+    """
+
+    logger = TestLogger(TESTNAME)
+
+    cycles = []
+
+    # generate makefile
+    mkf = Makefile()
+    mkf.add_fc_test_source("test.c")
+    mkf.add_cl_test_source("cluster.c")
+    mkf.add_cl_prog_source("mrbci/mrbci_params.c")
+    mkf.add_cl_prog_source("mrbci/mrbci.c")
+    mkf.add_cl_prog_source("mrbci/logm.c")
+    mkf.add_cl_prog_source("mrbci/feature_extraction.c")
+    mkf.add_cl_prog_source("mrbci/half_diag.c")
+    mkf.add_cl_prog_source("func/convert.c")
+    mkf.add_cl_prog_source("func/copy_mat.c")
+    mkf.add_cl_prog_source("linalg/matop_f.c")
+    mkf.add_cl_prog_source("linalg/svd.c")
+    mkf.add_cl_prog_source("linalg/svd_parallel.c")
+    mkf.add_define("PARALLEL")
+    mkf.write()
+
+    # generate the stimuli
+    X, Y = gen_stimuli()
+
+    # prepare header file
+    header = HeaderFile("test_stimuli.h")
+    # header.add(HeaderInclude("../../../../src/cl/func/functional.h"))
+    header.add(HeaderArray("x_stm", "int32_t", X.ravel()))
+    header.add(HeaderArray("y_exp", "int8_t", Y.ravel()))
+    header.write()
+
+    # compile and run
+    os.system("make clean all run > {}".format(RESULT_FILE))
+
+    # parse output
+    result = parse_output(RESULT_FILE)
+
+    # we skip this test, it is just for performance measurement
+    result["1"]["result"] = None
+
+    cycles.append(int(result['1']['cycles']))
+
+    casename = "logm + half_diag (par)"
+
+    # log the result
+    logger.show_subcase_result(casename, result)
+
+    # return summary
+    return logger.summary()
